a6ae66f332727f6160e002a3bbfd2e28502a378c
ssoper <ssoper@usgs.gov>
2020-06-08 10:22:56 -0500

    spacing and new lines

--- src/main/resources/sql/deleteDiscreteGroundWaterData.sql
+++ src/main/resources/sql/deleteDiscreteGroundWaterData.sql
@@ -1,8 +1,8 @@
 delete
 from discrete_ground_water
 where exists
-		  (select null
-		   from field_visit_header_info
-		   where json_data_id = ? and
-				   discrete_ground_water.field_visit_identifier = field_visit_header_info.field_visit_identifier
-		  )
+    (select null
+        from field_visit_header_info
+           where json_data_id = ?
+           and discrete_ground_water.field_visit_identifier = field_visit_header_info.field_visit_identifier
+    )
